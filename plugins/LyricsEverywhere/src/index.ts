b81a688d3ae0a7fea8d9379fde3be1dbd8a394e6
Jonas W <jonas@jxnxsdev.me>
2025-05-19 22:05:18 +0200

    Fix lyrics not updating when song natually transitions

--- plugins/LyricsEverywhere/src/index.ts
+++ plugins/LyricsEverywhere/src/index.ts
@@ -34,35 +34,31 @@ unloads.add(() => {
     document.querySelectorAll("#lyewLyrics").forEach(el => el.remove());
 });
 
-redux.intercept(
-    ["playbackControls/MEDIA_PRODUCT_TRANSITION", "playbackControls/SET_PLAYBACK_STATE"],
-    unloads,
-    async () => {
-        const mediaItem = await MediaItem.fromPlaybackContext();
-        if (!mediaItem) return;
 
-        const lyrics = await mediaItem.lyrics();
-        if (!lyrics) {
-            lyricsElement.innerHTML = `<strong>No lyrics loaded</strong>`;
-            lyricsElement.style.display = "none";
-            lyricsMap = new Map();
-            return;
-        }
-        lyricsElement.innerHTML = `...`;
-        lyricsElement.style.display = "block";
+MediaItem.onMediaTransition(unloads, async (mediaItem) => {
+    if (!mediaItem) return;
 
-        const map = new Map<number, string>();
-        for (const line of lyrics.subtitles.split("\n")) {
-            const [timePart, textPart] = line.split("]");
-            if (!textPart) continue;
-            const [min, sec] = timePart.replace("[", "").split(":").map(Number);
-            const timeInSec = Math.floor(min * 60 + sec);
-            map.set(timeInSec, textPart.trim());
-        }
-
-        lyricsMap = map;
+    const lyrics = await mediaItem.lyrics();
+    if (!lyrics) {
+        lyricsElement.innerHTML = `<strong>No lyrics loaded</strong>`;
+        lyricsElement.style.display = "none";
+        lyricsMap = new Map();
+        return;
     }
-);
+    lyricsElement.innerHTML = `...`;
+    lyricsElement.style.display = "block";
+
+    const map = new Map<number, string>();
+    for (const line of lyrics.subtitles.split("\n")) {
+        const [timePart, textPart] = line.split("]");
+        if (!textPart) continue;
+        const [min, sec] = timePart.replace("[", "").split(":").map(Number);
+        const timeInSec = Math.floor(min * 60 + sec);
+        map.set(timeInSec, textPart.trim());
+    }
+
+    lyricsMap = map;
+});
 
 
 function getClosestTime(currentTime: number): number | null {
