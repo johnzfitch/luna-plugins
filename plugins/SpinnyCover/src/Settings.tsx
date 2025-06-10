9d5934d276b4d26931cf1c47f6048f3eb4da6df9
Jonas W <jonas@jxnxsdev.me>
2025-06-10 21:19:58 +0200

    Add SpinnyCover

---
+++ plugins/SpinnyCover/src/Settings.tsx
@@ -0,0 +1,70 @@
+import React from "react";
+
+import { ReactiveStore } from "@luna/core";
+import { LunaSettings, LunaSwitchSetting, LunaNumberSetting } from "@luna/ui";
+
+export const storage = ReactiveStore.getStore("SpinnyCover");
+export const settings = await ReactiveStore.getPluginStorage("SpinnyCover", { spinSpeed: 10, blurPercentage: 50 });
+
+let lastSpeedNumber = settings.spinSpeed;
+
+export const Settings = () => {
+    const [spinSpeed, setSpinSpeed] = React.useState(settings.spinSpeed);
+    const [blurPercentage, setBlurPercentage] = React.useState(settings.blurPercentage);
+
+    React.useEffect(() => {
+        const invertedSpeed = 10000 - spinSpeed * 10;
+        document.documentElement.style.setProperty("--spinny-cover-speed", `${invertedSpeed}ms`);
+        document.documentElement.style.setProperty("--spinny-cover-blur", `${blurPercentage / 5}px`);
+
+    }, [spinSpeed, blurPercentage]);
+    return (
+        <LunaSettings>
+            <LunaNumberSetting
+                title="Spin Speed (1000 = no spin)"
+                desc="Speed of the spinning cover image."
+                tooltip="Spiiiiiiiiiiiiiiiiiiiiiiiiiiin"
+                value={spinSpeed}
+                min={1}
+                max={1000}
+                onNumber={(value: number) => {
+                    setSpinSpeed((settings.spinSpeed = value));
+                    const invertedSpeed = 10000 - spinSpeed * 10;
+                    document.documentElement.style.setProperty("--spinny-cover-speed", `${invertedSpeed}ms`);
+                    if (lastSpeedNumber !== value && lastSpeedNumber < 960 && value >= 960) {
+                        const audioPlayer = document.getElementById("spinnyCoverAudio") as HTMLAudioElement;
+                        if (audioPlayer) {
+                            audioPlayer.play().catch((error) => {
+                                console.error("Error playing audio:", error);
+                            });
+                        }
+
+                        const helicopterGif = document.getElementById("spinnyCoverHelicopter") as HTMLImageElement;
+                        if (helicopterGif) {
+                            helicopterGif.style.display = "block";
+                            setTimeout(() => {
+                                helicopterGif.style.display = "none";
+                            }, 5000);
+                        }
+                    }
+                    lastSpeedNumber = value;
+                }}
+            />
+            <LunaNumberSetting
+                title="Blur Percentage"
+                desc="Percentage of blur applied to the cover image."
+                tooltip="Blur it like you mean it"
+                min={0}
+                max={100}
+                value={blurPercentage}
+                onNumber={(value: number) => {
+                    setBlurPercentage((settings.blurPercentage = value));
+                    if (value < 0) setBlurPercentage((settings.blurPercentage = 0));
+                    if (value > 100) setBlurPercentage((settings.blurPercentage = 100));
+                    document.documentElement.style.setProperty("--spinny-cover-blur", `${value / 5}px`);
+                    console.log(`Blur percentage set to ${value}%`);
+                }}
+            />
+        </LunaSettings>
+    );
+};
---
+++ plugins/SpinnyCover/src/index.ts
@@ -0,0 +1,166 @@
+import type { LunaUnload } from "@luna/core";
+import { MediaItem } from "@luna/lib";
+import { settings } from "./Settings";
+
+const styles = document.createElement("style");
+document.head.appendChild(styles);
+
+styles.textContent = `
+#spinnyCoverBg, #spinnyCoverBgNext {
+    position: fixed;
+    top: 50%;
+    left: 50%;
+    width: 150vmax;
+    height: 150vmax;
+    transform: translate(-50%, -50%);
+    z-index: -1;
+    background-size: cover;
+    background-position: center;
+    filter: blur(var(--spinny-cover-blur));
+    animation: spinnyCoverAnimation var(--spinny-cover-speed) linear infinite;
+    pointer-events: none;
+    opacity: 1;
+    transition: opacity 1s ease-in-out;
+}
+
+#spinnyCoverBgNext {
+    position: absolute;
+    opacity: 0;
+    animation: none;
+}
+
+@keyframes spinnyCoverAnimation {
+    from {
+        transform: translate(-50%, -50%) rotate(0deg);
+    }
+    to {
+        transform: translate(-50%, -50%) rotate(360deg);
+    }
+}
+
+#wimp,
+main,
+[class^="_sidebarWrapper"],
+[class^="_mainContainer"],
+[class*="smallHeader"],
+#footerPlayer,
+#sidebar,
+[class^="_bar"],
+[class^="_sidebarItem"]:hover,
+.enable-scrollbar-styles ::-webkit-scrollbar-corner,
+.enable-scrollbar-styles ::-webkit-scrollbar-track {
+    background-color: color-mix(in srgb, var(--wave-color-solid-base-brighter), transparent 70%) !important;
+}
+
+#nowPlaying > [class^="_innerContainer"] {
+    height: calc(100vh - 126px);
+    overflow: hidden;
+}
+
+[class^="_bottomGradient"] {
+    display: none;
+}
+
+[class*="smallHeader--"]::before {
+    background-image: var(--cover-gradient) !important;
+    background-color: var(--wave-color-solid-base-brighter);
+    filter: unset;
+    background-blend-mode: normal;
+}
+
+[class*="emptyStateImage"] {
+    background-color: transparent !important;
+}
+
+[data-test="search-results-top"] > [class*="container"]::before,
+[data-test="search-results-normal"] > [class*="container"]::before {
+    background-image: var(--cover-gradient);
+    z-index: -1;
+    left: -36px;
+    right: -36px;
+    height: calc(var(--topSpacing) + 50px);
+}
+
+[data-test="search-results-normal"] > [class*="container"] > [class*="divider"],
+[data-test="search-results-top"] > [class*="container"] > [class*="divider"] {
+    display: none;
+}
+
+[data-test="search-results-top"] > [class*="container"] {
+    background-color: unset;
+}
+
+[class^="_tabListWrapper"] {
+    background-image: linear-gradient(180deg, rgb(var(--cover-DarkMuted)) 70%, transparent) !important;
+}
+`;
+
+const audioPlayer = document.createElement("audio");
+audioPlayer.id = "spinnyCoverAudio";
+audioPlayer.src = "https://www.myinstants.com/media/sounds/helicopter-helicopter-parakofer-parakofer.mp3";
+audioPlayer.volume = 1;
+audioPlayer.style.display = "none";
+document.body.appendChild(audioPlayer);
+
+const helicopterGif = document.createElement("img");
+helicopterGif.src = "https://media0.giphy.com/media/aWXhHFliOBOoZK89iu/giphy.gif";
+helicopterGif.id = "spinnyCoverHelicopter";
+helicopterGif.style.position = "fixed";
+helicopterGif.style.top = "50%";
+helicopterGif.style.left = "50%";
+helicopterGif.style.transform = "translate(-50%, -50%)";
+helicopterGif.style.zIndex = "9999";
+helicopterGif.style.display = "none";
+document.body.appendChild(helicopterGif);
+
+const coverBg = document.createElement("div");
+coverBg.id = "spinnyCoverBg";
+document.body.appendChild(coverBg);
+
+const coverBgNext = document.createElement("div");
+coverBgNext.id = "spinnyCoverBgNext";
+document.body.appendChild(coverBgNext);
+
+const invertedSpeed = 10000 - settings.spinSpeed * 10;
+document.documentElement.style.setProperty("--spinny-cover-speed", `${invertedSpeed}ms`);
+document.documentElement.style.setProperty("--spinny-cover-blur", `${settings.blurPercentage / 5}px`);
+
+async function setInitialCover() {
+    let mediaItem = await MediaItem.fromPlaybackContext();
+    if (!mediaItem) return;
+    const url = await mediaItem.coverUrl();
+    if (!url) return;
+    coverBg.style.backgroundImage = `url("${url}")`;
+}
+setInitialCover();
+
+MediaItem.onMediaTransition(new Set(), async (mediaItem) => {
+    if (!mediaItem) return;
+    const url = await mediaItem.coverUrl();
+    if (!url) return;
+    coverBgNext.style.backgroundImage = `url("${url}")`;
+    coverBgNext.style.opacity = "1";
+    coverBg.style.opacity = "0";
+    setTimeout(() => {
+        coverBg.style.backgroundImage = `url("${url}")`;
+        coverBg.style.opacity = "1";
+        coverBgNext.style.opacity = "0";
+    }, 100);
+});
+
+export { Settings } from "./Settings";
+
+export const unloads = new Set<LunaUnload>();
+unloads.add(() => {
+    styles.remove();
+    coverBg.remove();
+    coverBgNext.remove();
+    audioPlayer.remove();
+    helicopterGif.remove();
+    document.documentElement.style.removeProperty("--spinny-cover-speed");
+    document.documentElement.style.removeProperty("--spinny-cover-blur");
+    console.log("Unloading SpinnyCover");
+    document.querySelectorAll("#spinnyCoverAudio").forEach(el => el.remove());
+    document.querySelectorAll("#spinnyCoverBg, #spinnyCoverBgNext").forEach(el => el.remove());
+
+});
